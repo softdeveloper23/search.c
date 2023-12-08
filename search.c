78fa77e2d7e66048eafd83468bc0e5c9618f831e
unknown <brannongarrett77@gmail.com>
2023-12-08 16:45:49 +0100

    Updated code

--- search.c
+++ search.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include "cs50.h"
 
 int main(void)
 {
     int numbers[] = {20, 500, 10, 5, 100, 1, 50};
 
-    int n = 75;
+    int n = get_int("Enter number: ");
     for (int i = 0; i < 7; i++)
     {
         if (numbers[i] == n)
